e903107d624dc0111c51cc9b235998a7a5a9795b
Dave <david.quinta415@gmail.com>
2022-04-27 10:50:28 -0600

    :boom: Cardano Connection Config files

---
+++ config.js
@@ -0,0 +1,5 @@
+const blockfrostApiKey = {
+    0: "testnettl09lDvIe2gqsUQgGp0XA6WWXxDyctBc", // testnet
+    1: "mainnetfqH0CVlBesnsj5IKhgIYCn231KzqUOyk" // mainnet
+}
+export default blockfrostApiKey;
