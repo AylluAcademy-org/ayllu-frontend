eb8a900b8acf36f9e1f0655b5a27da17cfd89061
Dave <david.quinta415@gmail.com>
2022-05-01 22:53:21 -0600

    Revert "Merge branch 'feature/walletconnect' into develop"
    This reverts commit 2b708006f2595baa9335efe26c4480af0e84c0dc.

--- config.js
+++
@@ -1,5 +0,0 @@
-const blockfrostApiKey = {
-    0: "testnettl09lDvIe2gqsUQgGp0XA6WWXxDyctBc", // testnet
-    1: "mainnetfqH0CVlBesnsj5IKhgIYCn231KzqUOyk" // mainnet
-}
-export default blockfrostApiKey;
--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    webpack5:false,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
-    },
-    reactStrictMode: true,
-    webpack: config => {
-      config.module.rules.push({
-        test: /\.wasm$/,
-        loaders: ['wasm-loader'],
-        type:  "javascript/auto",
-      })
-      return config
     },
 }
